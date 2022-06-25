a4f4075787f6a56536cd7cf989ea2bbe9128641e
Victor Wen <vicwen02@g.ucla.edu>
2022-06-25 14:27:37 -0400

    dev art

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Physics Fields
+    // Movement Fields
     public float moveSpeed = 3;
     private Vector2 inputVector = new Vector2();
     private Rigidbody2D body;
